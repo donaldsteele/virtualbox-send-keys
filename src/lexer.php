8f91ec6d3ea0047ded0e9a19327bff31481a5c95
Don <don.steele@gmail.com>
2017-04-23 17:10:17 -0400

    Added the ability to pass in a script as a source for to send to the target vm

--- src/lexer.php
+++ src/lexer.php
@@ -13,11 +13,6 @@
 class lexer
 {
 
-    /***
-     * @var string[] list of positions that we should pause / wait at
-     */
-    public static $wait;
-
 
     /***
      * @param $string
@@ -40,37 +35,11 @@ class lexer
 
         $out = str_ireplace($returns, chr(10), $string);
         $out = str_ireplace($tabs, chr(9), $out);
-        $out = self::findScriptTerms($out);
-
-        return $out;
-    }
-
-
-    static private function findScriptTerms($string)
-    {
-
-        //only support the wait function for now, will expand this more at a later date
-        $re = '/\^WAIT\[([0-9]+)\]/i';
-
-        // get positions of command
-        if (preg_match_all($re, $string, $matches, PREG_OFFSET_CAPTURE)) {
-            // Print the entire match result
-            // now that we have the positions recorded we can remove them from our input string.
-            $out = preg_replace($re, '', $string);
-
-            foreach ($matches[0] as $matchToken => $matchDetails) {
-
-                $watchStart = $matchDetails[1] - (strlen('^WAIT[]') * $matchToken);
-                // look in the second array [1] and get the corrisponding value inside our regex () match in this case the number passed to ^WAIT
-                $watchValue = $matches[1][$matchToken][0];
-                self::$wait[$watchStart] = $watchValue;
-            }
-
-        } else {
-            $out = $string;
-        }
 
 
         return $out;
     }
+
+
+
 }
